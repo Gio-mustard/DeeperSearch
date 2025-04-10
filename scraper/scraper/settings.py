623fb04b9ed60ae0e4741d86aa8f6a5057400778
morquecho-s <mssm011009@gmail.com>
2025-04-09 18:09:35 -0700

    delete: a item pipelines settings for scraper module

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -63,9 +63,10 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   "scraper.pipelines.PagePipeline": 300,
-}
+
+# ITEM_PIPELINES = { ! Lo puse en PageSpider
+#    "scraper.scraper.pipelines.PagePipeline": 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
